dea31daca58cebf584e00b74a9c468b724839a59
Athul-Krishna <athulkrishnaks1998@gmail.com>
2021-08-17 18:48:07 +0530

    configure snapshotting

--- ProductsService/src/main/java/com/transac/ProductsService/ProductsServiceApplication.java
+++ ProductsService/src/main/java/com/transac/ProductsService/ProductsServiceApplication.java
@@ -5,11 +5,15 @@ import com.transac.ProductsService.core.errorhandling.ProductsServiceEventsError
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.PropagatingErrorHandler;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -30,4 +34,9 @@ public class ProductsServiceApplication {
 //		config.registerListenerInvocationErrorHandler("product-group", conf -> PropagatingErrorHandler.instance());
 	}
 
+	@Bean(name = "productSnapshotTriggerDefinition")
+	public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+		return new EventCountSnapshotTriggerDefinition(snapshotter, 6);
+		// Creating snapshot itself is considered as 1 event
+	}
 }
--- ProductsService/src/main/java/com/transac/ProductsService/command/ProductAggregate.java
+++ ProductsService/src/main/java/com/transac/ProductsService/command/ProductAggregate.java
@@ -14,7 +14,7 @@ import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 
-@Aggregate
+@Aggregate(snapshotTriggerDefinition = "productSnapshotTriggerDefinition")
 public class ProductAggregate {
 
     @AggregateIdentifier
